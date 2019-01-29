1d0683369518b9162d729fc5cd29e865d9206f65
Raghav Surya <raghavsurya@bgl-mac-009662.bgl.financial.local>
2019-01-29 16:43:44 +0000

    initial commit

---
+++ exercises/fib/index.js
@@ -0,0 +1,31 @@
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+function fib(n) {
+    let arr = [0];
+    for (let i=1; i <= n; i++) {
+        if(arr[i-2] !== undefined) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        else {
+            arr[i] = i;
+        }
+    }
+    return arr[n];
+}
+let arr = [0];
+function fib_recursive(n, i) {
+    if(n < 2 ) {
+        return n
+    }
+    return fib_recursive(n - 1) + fib_recursive(n - 2);
+}
+
+module.exports = fib;
